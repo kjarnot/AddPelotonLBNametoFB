30bc69682a583d8344887804eea16bbf8135845e
Kevin Jarnot <kjarnot@users.noreply.github.com>
2016-03-21 11:22:30 -0400

    Move JSON input file to github

--- AddPelotonLBNametoFacebook.user.js
+++ AddPelotonLBNametoFacebook.user.js
@@ -1,8 +1,10 @@
 // ==UserScript==
 // @name        AddPelotonLBNametoFB
+// @downloadURL https://github.com/kjarnot/AddPelotonLBNametoFB/raw/master/AddPelotonLBNametoFacebook.user.js
 // @namespace   http://www.jarnot.com
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
-// @require     http://courses.ischool.berkeley.edu/i290-4/f09/resources/gm_jq_xhr.js
+// @require     http://courses.ischool.berkeley.edu/i290-4/f09/resources/console.jq_xhr.js
+// @require     https://github.com/kjarnot/AddPelotonLBNametoFB/raw/master/peloton_name_map.json
 // @description Display LB name next to Facebook full user name
 // @include     http://facebook.com/*
 // @include     http://*.facebook.com/*
@@ -19,7 +21,7 @@ var version = '1.0.0';
 var version_timestamp = 1458051466388; // javascript:window.alert(new Date().getTime());
 var release_date = 20160316;
 
-var jsonNameMapURL = "http://playground.jarnot.com/peloton/peloton_name_map.json";
+var jsonNameMapURL = "https://github.com/kjarnot/AddPelotonLBNametoFB/raw/master/peloton_name_map.json";
 var jsonNameMap = {};
 
 // Global variable to track # of FB names found in DOM
@@ -38,7 +40,7 @@ function checkMutationForNewComments(mutation) {
         addLBNames(addedNode);
     }
 
-    GM_log("Mutation!");
+    console.log("Mutation!");
 
     return false;
 }
@@ -55,7 +57,7 @@ if (typeof MutationObserver !== 'undefined') {
 function addLBNames(root) {
     var updatedNumFBNames = 0;
 
-    GM_log("In addLBNames()");
+    console.log("In addLBNames()");
 
     // Only run on Peloton-related pages
     if (/Peloton/i.test(document.title) === false) {
@@ -63,21 +65,21 @@ function addLBNames(root) {
     }
 
     var fbNames = document.evaluate('//a[@class=" UFICommentActorName"]', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-    GM_log("Found " + fbNames.snapshotLength + " comments");
+    console.log("Found " + fbNames.snapshotLength + " comments");
 
     updatedNumFBNames = fbNames.snapshotLength;
     if (updatedNumFBNames == numFBNames) {
-        GM_log("# of FB names has not changed.  Exiting addLBNames()...");
+        console.log("# of FB names has not changed.  Exiting addLBNames()...");
         return false;
     }
 
     for (var i = fbNames.snapshotLength - 1; i >= 0; i--) {
         var fbName = fbNames.snapshotItem(i);
         var nameText = fbName.innerText;
-        GM_log("Found FB name: " + nameText);
+        console.log("Found FB name: " + nameText);
         var LBName = jsonNameMap[nameText];
         if (LBName) {
-            GM_log("Setting name to " + LBName);
+            console.log("Setting name to " + LBName);
             var newName = nameText + " (#" + LBName + ")";
             fbName.innerText = newName;
         }
@@ -95,8 +97,8 @@ $.ajax({
     dataType: 'json',
     success: function(data) {
         jsonNameMap = data;
-        //GM_log("Dumping name map...");
-        //GM_log(jsonNameMap);
+        //console.log("Dumping name map...");
+        //console.log(jsonNameMap);
 
         addLBNames(document.body);
 
